73d52634209c1c63f4c69ed6b96a63a49c6d1064
Ansh Patel <anshpatel6588@gmail.com>
2024-11-28 20:20:22 -0500

    player added

---
+++ Final_Project/Player.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public class Player
+    {
+        public Vector2 Position { get; private set; }
+        public float Speed { get; set; }
+        private int windowWidth;
+        private int windowHeight;
+        private int radius;
+
+
+        public Player(Vector2 initialPosition, float speed, int windowWidth, int windowHeight)
+        {
+            Position = initialPosition;
+            Speed = speed;
+            this.windowWidth = windowWidth;
+            this.windowHeight = windowHeight;
+
+        }
+
+        public void Update(GameTime gameTime, List<Rectangle> walls)
+        {
+            KeyboardState state = Keyboard.GetState();
+            Vector2 newPosition = Position;
+            if (state.IsKeyDown(Keys.Left) || state.IsKeyDown(Keys.A))
+            {
+                newPosition.X -= Speed;
+                if (newPosition.X < 0)
+                {
+                    newPosition.X = 0;
+                }
+            }
+            if (state.IsKeyDown(Keys.Right) || state.IsKeyDown(Keys.D))
+            {
+                newPosition.X += Speed;
+                if (newPosition.X > windowWidth - radius * 2)
+                {
+                    newPosition.X = windowWidth - radius * 2;
+                }
+            }
+            if (state.IsKeyDown(Keys.Up) || state.IsKeyDown(Keys.W))
+            {
+                newPosition.Y -= Speed;
+                if (newPosition.Y < 0)
+                {
+                    newPosition.Y = 0;
+                }
+            }
+            if (state.IsKeyDown(Keys.Down) || state.IsKeyDown(Keys.S))
+            {
+                newPosition.Y += Speed;
+                if (newPosition.Y > windowHeight - radius * 2)
+                {
+                    newPosition.Y = windowHeight - radius * 2;
+                }
+
+            }
+
+
+            Rectangle playerRectangle = new Rectangle((int)newPosition.X, (int)newPosition.Y, 50, 50);
+            bool collidesWithWall = false;
+
+            foreach (var wall in walls)
+            {
+                if (playerRectangle.Intersects(wall))
+                {
+                    collidesWithWall = true;
+                    break;
+                }
+            }
+
+
+            if (!collidesWithWall)
+            {
+                Position = newPosition;
+            }
+        }
+    }
+}
--- Final_Project/obj/Debug/net8.0/Final_Project.AssemblyInfo.cs
+++ Final_Project/obj/Debug/net8.0/Final_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2c09f47d5cfc343661ecede9535b482767e07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19319df3149293a80d402e69bb7bc13aa5795557")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
